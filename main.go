d91805685164099fce5a38795fb5bb3ead82f8ff
soramon0 <23435312+soramon0@users.noreply.github.com>
2026-04-02 19:48:19 +0100

    done

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":" + port) // Listens on port 8080
+}
